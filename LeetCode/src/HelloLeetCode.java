baede5fd3a4c7b13f0e006dd33d394ebababfbdc
CRH <crh1028vip@163.com>
2017-11-14 15:34:11 +0800

    leetCode

---
+++ LeetCode/src/HelloLeetCode.java
@@ -0,0 +1,10 @@
+/**
+ * Created by githu on 2017/11/14.
+ */
+public class HelloLeetCode {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Leet Code");
+    }
+}
+
