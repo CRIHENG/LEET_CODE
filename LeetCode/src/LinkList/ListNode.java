af5fb7f7474e62ef222bf4ab40e9d9d089fe5b15
CRH <crh1028vip@163.com>
2017-11-14 15:48:31 +0800

    leetCode

---
+++ LeetCode/src/LinkList/ListNode.java
@@ -0,0 +1,15 @@
+package LinkList;
+
+/**
+ * Created by githu on 2017/11/10.
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+}
